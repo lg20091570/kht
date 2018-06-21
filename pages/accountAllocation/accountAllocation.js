2a9c892bca3ed6ceb4e0cedf314dfb87f9917094
lg20091570 <lg18788129790>
2018-06-21 16:24:01 +0800

    新增页面

--- pages/accountAllocation/accountAllocation.js
+++ pages/accountAllocation/accountAllocation.js
@@ -1,37 +1,104 @@
+// pages/visitCustomerstate/visitCustomerstate.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    focus: false,
-    inputValue: ''
+    min: 5,//最少字数  
+    max: 80, //最多字数 
+    array: ['组织1', '组织2', '组织3', '组织4'],
+    deal: ['有权限', '无权限']
   },
-  bindKeyInput: function (e) {
+  //字数限制    
+  inputs: function (e) {
+    // 获取输入框的内容  
+    var value = e.detail.value;
+    // 获取输入框内容的长度  
+    var len = parseInt(value.length);
+
+    //最少字数限制  
+    if (len <= this.data.min)
+      this.setData({
+        texts: ""
+      })
+    else if (len > this.data.min)
+      this.setData({
+        texts: " "
+      })
+
+    //最多字数限制  
+    if (len > this.data.max) return;
+    // 当输入框内容的长度大于最大长度限制（max)时，终止setData()的执行  
     this.setData({
-      inputValue: e.detail.value
+      currentWordNumber: len //当前字数    
+    });
+  },
+
+
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value
     })
   },
-  bindReplaceInput: function (e) {
-    var value = e.detail.value
-    var pos = e.detail.cursor
-    var left
-    if (pos !== -1) {
-      // 光标在中间
-      left = e.detail.value.slice(0, pos)
-      // 计算光标的位置
-      pos = left.replace(/11/g, '2').length
-    }
-
-    // 直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
-    return {
-      value: value.replace(/11/g, '2'),
-      cursor: pos
-    }
-
-    // 或者直接返回字符串,光标在最后边
-    // return value.replace(/11/g,'2'),
+  dealPickerChange: function (e) {
+    this.setData({
+      page: e.detail.value
+    })
   },
-  bindHideKeyboard: function (e) {
-    if (e.detail.value === '123') {
-      // 收起键盘
-      wx.hideKeyboard()
-    }
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
