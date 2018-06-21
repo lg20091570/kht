89295d1c5e4d3ff8e75e4353217af2ceb653d745
lg20091570 <lg18788129790>
2018-06-21 21:08:13 +0800

    新增页面

--- pages/enterpriseSetting/enterpriseSetting.js
+++ pages/enterpriseSetting/enterpriseSetting.js
@@ -1,12 +1,12 @@
-// pages/businessManagement/businessManagement.js
+// pages/enterpriseSetting/enterpriseSetting.js
 Page({
 
   /**
    * 页面的初始数据
    */
-  // data: {
-
- 
+  data: {
+  
+  },
 
   /**
    * 生命周期函数--监听页面加载
